6abadaa5af5db9c8ea982e7c595c6dccc5b70297
Anvesh <136995186+findeme1@users.noreply.github.com>
2024-07-10 11:26:00 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/signin";
+import SignUp from "./pages/signup";
+import About from "./pages/about";
+import Profile from "./pages/profile";
+import Home from "./pages/home";
 
 export default function App() {
-  return <h1 className="text-red-300">App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/home.jsx
@@ -0,0 +1,3 @@
+export default function Homeome() {
+  return <div>Home</div>;
+}
