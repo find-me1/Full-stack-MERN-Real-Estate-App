184d4ef3d867dc3aafaa79d041695cad69f71a01
Anvesh <136995186+findeme1@users.noreply.github.com>
2025-03-18 16:15:47 +0530

    server setup

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,3 +1,11 @@
-export default function Homeome() {
-  return <div>Home</div>;
-}
+import { LinkPreview } from "@dhaiwat10/react-link-preview";
+
+const Home = () => {
+  return (
+    <LinkPreview
+      url="https://youtu.be/ndp8jnl0-Ug?si=mrq_5zVxDK3UFmQB"
+      width="400px"
+    />
+  );
+};
+export default Home;
